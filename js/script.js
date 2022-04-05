0958592a5a554caae581794b5cf280287106d71b
Viktoriia <egorovaviktoriia0@gmail.com>
2022-04-05 22:52:59 +0200

    JS operators

--- js/script.js
+++ js/script.js
@@ -1,10 +1,37 @@
 "use strict";
-let number = 5;
-console.log(4 / 0);
-console.log('strigng' * 9);
+// Интерполяция 
+const category = 'toys';
+console.log(`http//someurl.com/${category} / 5`);
 
-const persone = 'Alex'; 
+const user = "Ivan"; 
+alert(`Привет, ${user}`);
 
-const bool = true;
+// Конкотинация 
 
-console.log(something);
+console.log('arr' + ' - object');
+console.log(4 + '- object');
+
+
+//  инкремент и декремент 
+let incr = 10,
+    decr = 10; 
+    
+// ++incr; 
+// --decr; 
+console.log(incr++); 
+console.log(decr--);
+
+//  Оператор %
+
+console.log(5 % 2);
+
+// присваивание и сравнение 
+
+console.log(2 * 4 === '8');
+
+// Операторы && || 
+
+const isChecked = true,
+    isClosed = false; 
+
+console.log(isChecked || !isClosed);
